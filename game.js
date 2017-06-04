42fc3b4fbeb5d14c15872a297da780e9bd93bbd7
Edwin Marshall <emarshall85@gmail.com>
2017-06-04 13:03:21 -0400

    Epic is done.

--- game.js
+++ game.js
@@ -14,43 +14,36 @@ function game() {
     const computerChoice = choices[index];
 
     const playerChoice = document.getElementById("player").value;
-    let playerWins = false;
+    // Step One: Declare a variable to keep track of whether or not the player
+    // won or lost
 
-    // Step One: Complete the following if statement such that it properly
-    // updates the playerWins variable after comparing the computer's choice to
-    // the player's choice. There are three branches here because of the fact
-    // that there are only three cases in which the player can win (since the
-    // computer wins ties!). Remember, paper beats rock, rock beats scissors,
-    // scissors beat paper. 
+    // Step Two: Write conditional logic which compares the player's choice with
+    // the computer's and properly updates the variable you declared in step
+    // one.
     //
-    // Hints:
-    // - you can combine logical expressions with && and ||, where && means
-    // "and" and || means "or". Thus, "false || true" would evaluate to true
-    // because only one value needs to be truthy, while "false && true" would
-    // evalulate to false because both values need to be truthy.
+    // Hints: - you can combine logical expressions with && and ||, where &&
+    // means "and" and || means "or". Thus, "false || true" would evaluate to
+    // true because only one value needs to be truthy, while "false && true"
+    // would evalulate to false because both values need to be truthy.
     //
     // - given the above, this three-part if statement could be written as a
     // single, long conditional expression inside of one if statement. Doing so
     // would be rather difficult to read, however, which is why we have three
     // separate if statements chained together
-    if () {
-    } else if () {
-    } else if () {
-    }
 
-    // Step Two: Fix the following string such that the value of computerChoice
-    // is inserted after the word "chose" but before the period. That is, if the
-    // computer chose "rock", message should have the value "The computer chose
-    // rock.\n"
+    // Step Three: Fix the following string such that the value of
+    // computerChoice is inserted after the word "chose" but before the period.
+    // That is, if the computer chose "rock", message should have the value "The
+    // computer chose rock.\n"
     let message = "The computer chose .\n";
 
-    // Step Three: Write an if statement that, when the player wins, would
-    // append "Congrats, you win!" onto the end of message above, and "Sorry,
-    // you lose!" when the player loses. As an example, when a player loses,
-    // message should look like this (note the newline):
+    // Step Four: Write an if statement that, when the player wins, would append
+    // "Congrats, you win!" onto the end of message above, and "Sorry, you
+    // lose!" when the player loses. As an example, when a player loses, message
+    // should look like this (note the newline):
     //
-    // The computer chose rock.
-    // Sorry, you lose!
+    // The computer chose rock.  Sorry, you lose!
+    
 
-    alert(message);
+    // Step Five: Open a modal which displays this message to the user.
 }
